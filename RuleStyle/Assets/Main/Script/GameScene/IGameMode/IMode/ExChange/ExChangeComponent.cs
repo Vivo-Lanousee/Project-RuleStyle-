2d5d2428905dee21b36bf9a38fd9a4ed2669ed32
504-40\ktc <ktc23a32a0003@edu.kyoto-tech.ac.jp>
2025-02-12 14:14:33 +0900

    現在のキャラクター３Dモデルとゲーム

---
+++ RuleStyle/Assets/Main/Script/GameScene/IGameMode/IMode/ExChange/ExChangeComponent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rule変更画面のコンポーネント
+/// </summary>
+public class ExChangeComponent : MonoBehaviour
+{
+    //Playerの細かい対応UIはRuleComponent内に。
+    //UI_Xのようなナンバーは対応するUserがいない場合消失させるものとする。
+    public GameObject UI_One;
+    public Rule_UI_RuleComponent Player_One;
+    public GameObject UI_Two;
+    public Rule_UI_RuleComponent Player_Two;
+    public GameObject UI_Three;
+    public Rule_UI_RuleComponent Player_Three;
+    public GameObject UI_Four;
+    public Rule_UI_RuleComponent Player_Four;
+}
---
+++ RuleStyle/Assets/Main/Script/GameScene/IGameMode/IMode/ExChange/GameMode_ExchangeMode.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+/// <summary>
+/// カード変更シーンの設定。
+/// </summary>
+public class GameMode_ExchangeMode : IGameMode
+{
+    void IGameMode.Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void IGameMode.FixUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// カードの情報とイベント付けを全て行う。
+    /// </summary>
+    /// <exception cref="System.NotImplementedException"></exception>
+    void IGameMode.Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void IGameMode.Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
